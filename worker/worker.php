fe60c1daeed82c6d031ebeb366cc1b8c78e8e765
Andrey Rocha <ahrocha@gmail.com>
2025-04-07 14:02:43 -0300

    Configuração inicial de Docker e diretórios.

---
+++ worker/worker.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "[WORKER] Iniciando...\n";
+
+while (true) {
+    // simulação
+    echo "[WORKER] Aguardando mensagem...\n";
+
+    sleep(5);
+
+    echo "[WORKER] Processou mensagem de notificação\n";
+}
